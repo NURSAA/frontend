d77785d9da1b039859aa9f91eafdbd8d3c2de01d
musialpatryk <musial.patryk10@gmail.com>
2022-06-04 21:10:59 +0200

    Add tables details

--- src/app/views/private/tables/tables.component.ts
+++ src/app/views/private/tables/tables.component.ts
@@ -3,7 +3,9 @@ import {MockService} from 'src/app/services/mock.service';
 import {IFloor} from 'src/app/_types/floor';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {ToastsService} from 'src/app/modules/toasts/toasts.service';
-import {ActivatedRoute} from '@angular/router';
+import {
+    RestaurantDetailsComponent
+} from 'src/app/views/private/restaurants/restaurant-details/restaurant-details.component';
 
 type IFloorExtended = IFloor & {collapsed: boolean};
 
@@ -15,24 +17,31 @@ export class TablesComponent implements OnInit {
     floors!: IFloorExtended[];
 
     isFloorModalOpen = false;
-    floorForm = new FormGroup({
-        name: new FormControl(null, Validators.required)
-    })
+    floorForm!: FormGroup;
 
     constructor(
         private mockService: MockService,
         private toastService: ToastsService,
-        private route: ActivatedRoute
+        private restaurantDetails: RestaurantDetailsComponent
     ) {
     }
 
     ngOnInit(): void {
+        this.createForm();
         this.loadFloors();
     }
 
+    private createForm(): void {
+        this.floorForm = new FormGroup({
+            name: new FormControl(null, Validators.required),
+            restaurant: new FormControl(null, Validators.required),
+            level: new FormControl(null, Validators.required)
+        });
+        this.floorForm.get('restaurant')?.disable();
+    }
+
     private loadFloors(): void {
-        const restaurantId = Number(this.route.snapshot.parent?.params['id']);
-        this.mockService.getAll('floors', {'restaurant.id': restaurantId})
+        this.mockService.getAll('floors', {'restaurant.id': this.restaurantDetails.restaurantId})
             .subscribe((floors) => {
                 this.floors = floors;
             });
@@ -40,12 +49,19 @@ export class TablesComponent implements OnInit {
 
     openFloorModal(): void {
         this.isFloorModalOpen = true;
-        this.floorForm.reset();
+        this.floorForm.reset({
+            restaurant: this.restaurantDetails.restaurant.name
+        });
     }
 
 
     saveFloor(): void {
-        this.mockService.persist('floors', this.floorForm.value)
+        const payload = {
+            ...this.floorForm.value,
+            restaurant: this.restaurantDetails.restaurant,
+            tables: []
+        }
+        this.mockService.persist('floors', payload)
             .subscribe(() => {
                 this.toastService.saved();
                 this.isFloorModalOpen = false;
