479ad8469e4f4d1490603e2b5c0ed6e0064421eb
musialpatryk <musial.patryk10@gmail.com>
2023-01-21 19:29:21 +0100

    Use proper id to create new dish order link

--- src/app/_types/menu.ts
+++ src/app/_types/menu.ts
@@ -1,8 +1,13 @@
 import {IRestaurant} from 'src/app/_types/restaurant';
 
+export const MENU_STATUS = {
+    ACTIVE: 'active',
+    INACTIVE: 'inactive',
+} as const;
 
 export interface IMenu {
     id?: number;
     name: string;
     restaurant: IRestaurant;
+    status: typeof MENU_STATUS[keyof typeof MENU_STATUS];
 }
