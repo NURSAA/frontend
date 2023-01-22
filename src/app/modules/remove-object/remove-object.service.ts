afe579816ef9480d3b90cf93c2b5cba1827fd3ba
musialpatryk <musial.patryk10@gmail.com>
2023-01-22 23:40:42 +0100

    Fix styles

--- src/app/modules/remove-object/remove-object.service.ts
+++ src/app/modules/remove-object/remove-object.service.ts
@@ -1,4 +1,4 @@
-import {Injectable, Input} from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Observable, Subject} from 'rxjs';
 import {IRestObject} from 'src/app/modules/rest/rest-object';
 
