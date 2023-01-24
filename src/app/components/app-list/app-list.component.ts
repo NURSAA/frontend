cbf650cfbcd047364ecb0b5164dd751070e7624a
musialpatryk <musial.patryk10@gmail.com>
2023-01-24 14:15:31 +0100

    Add pagination control

--- src/app/components/app-list/app-list.component.ts
+++ src/app/components/app-list/app-list.component.ts
@@ -18,6 +18,11 @@ export class AppListComponent implements OnInit, OnDestroy {
 
     listItems!: IRestCollection<string>;
 
+    hasPagination = false;
+    page = 1;
+    lastPage = 1;
+
+    private readonly PER_PAGE = 30;
     private _destroy$ = new Subject<void>();
 
     constructor(
@@ -41,10 +46,58 @@ export class AppListComponent implements OnInit, OnDestroy {
     }
 
     private loadData(): void {
+        this.addPaginationQuery();
         this.restClient.getAll(this.endpoint, this.queryObject)
             .subscribe((items) => {
+                this.computePagination(items)
                 this.listItems = items;
             });
+    }
+
+    private addPaginationQuery(): void {
+        if (!this.hasPagination) {
+            return;
+        }
+
+        const paginationQuery = {
+            page: this.page
+        };
+
+        Object.assign(this.queryObject, paginationQuery);
+    }
+
+    private computePagination(collection: IRestCollection<string>): void {
+        console.log(collection.hydra);
+        const totalItems = collection.hydra?.['hydra:totalItems'] as number;
+
+        if (!totalItems) {
+            return;
+        }
+
+        if (
+            collection.length < totalItems
+            && !this.hasPagination
+        ) {
+            this.hasPagination = true;
+            this.lastPage = Math.ceil(totalItems / this.PER_PAGE);
+            return;
+        }
+    }
+
+    changePage(action: 'next' | 'prev'): void {
+        let newPage = this.page;
+        if (action === 'next') {
+            newPage++;
+        } else {
+            newPage--;
+        }
+
+        if (newPage < 1 || newPage > this.lastPage) {
+            return;
+        }
+
+        this.page = newPage;
+        this.loadData();
     }
 
     ngOnDestroy(): void {
