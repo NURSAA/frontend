b3d3d0d8eb8acf7fbb27a0ccdfb78f6fd1bf483a
NURSAA <63779425+NURSAA@users.noreply.github.com>
2023-01-25 18:53:29 +0100

    Add Missing & Polish Translations

--- src/app/modules/translate/translations/pl.ts
+++ src/app/modules/translate/translations/pl.ts
@@ -1,4 +1,325 @@
 import {ITranslation} from 'src/app/modules/translate/interfaces';
 
 
-export const plTranslations: ITranslation[] = [];
+export const plTranslations: ITranslation[] = [
+     {
+        key: 'required',
+        value: 'Pole jest wymagane'
+    },
+    {
+        key: 'email',
+        value: 'Użyj poprawnego adresu e-mail'
+    },
+    {
+        key: 'LOGIN_PAGE',
+        value: 'Strona Logowania'
+    },
+    {
+        key: 'LOGIN',
+        value: 'Zaloguj'
+    },
+    {
+        key: 'ERROR',
+        value: 'Błąd'
+    },
+    {
+        key: 'ERROR_DESCRIPTION',
+        value: 'Coś poszło nie tak.'
+    },
+    {
+        key: 'OR_REGISTER',
+        value: 'lub utwórz nowe konto.'
+    },
+    {
+        key: 'REGISTER_PAGE',
+        value: 'Zarejestruj'
+    },
+    {
+        key: 'CREATE_NEW_ACCOUNT',
+        value: 'Utwórz nowe konto'
+    },
+    {
+        key: 'BRAND_NAME',
+        value: 'E-restaurant'
+    },
+    {
+        key: 'RESTAURANTS',
+        value: 'Restauracje'
+    },
+    {
+        key: 'ADD_RESTAURANT',
+        value: 'Dodaj'
+    },
+    {
+        key: 'DETAILS',
+        value: 'Szczegóły'
+    },
+    {
+        key: 'NAME',
+        value: 'Nazwa'
+    },
+    {
+        key: 'DESCRIPTION',
+        value: 'Opis'
+    },
+    {
+        key: 'URL',
+        value: 'URL'
+    },
+    {
+        key: 'LOGOUT',
+        value: 'Wyloguj'
+    },
+    {
+        key: 'MENUS',
+        value: 'Menu'
+    },
+    {
+        key: 'INGREDIENTS',
+        value: 'Składniki'
+    },
+    {
+        key: 'RESERVATIONS',
+        value: 'Rezerwacje'
+    },
+    {
+        key: 'TABLES',
+        value: 'Stoły'
+    },
+    {
+        key: 'SAVE',
+        value: 'Zapisz'
+    },
+    {
+        key: 'CLOSE_MODAL',
+        value: 'Zamknij'
+    },
+    {
+        key: 'RESTAURANT',
+        value: 'Restauracja'
+    },
+    {
+        key: 'ADD_INGREDIENT',
+        value: 'Dodaj Składnik'
+    },
+    {
+        key: 'ADD_INGREDIENT_GROUP',
+        value: 'Dodaj Grupę Składników'
+    },
+    {
+        key: 'ADD_MENU',
+        value: 'Dodaj Menu'
+    },
+    {
+        key: 'MENU_DETAILS',
+        value: 'Szczegóły Menu'
+    },
+    {
+        key: 'ADD_SECTION',
+        value: 'Dodaj Sekcję'
+    },
+    {
+        key: 'ZL',
+        value: 'ZŁ'
+    },
+    {
+        key: 'EDIT_MENU',
+        value: 'Edytuj Menu'
+    },
+    {
+        key: 'ADD_DISH',
+        value: 'Dodaj Danie'
+    },
+    {
+        key: 'CHOOSE_OPTION',
+        value: 'Wybierz Opcję'
+    },
+    {
+        key: 'FLOOR',
+        value: 'Piętro'
+    },
+    {
+        key: 'TABLE',
+        value: 'Stolik'
+    },
+    {
+        key: 'ADD_FLOOR',
+        value: 'Dodaj Piętro'
+    },
+    {
+        key: 'ADD_TABLE',
+        value: 'Dodaj Stolik'
+    },
+    {
+        key: 'RESERVATION_DETAILS',
+        value: 'Szczegóły Rezerwacji'
+    },
+    {
+        key: 'ORDERS',
+        value: 'Zamówienia'
+    },
+    {
+        key: 'USERNAME',
+        value: 'Użytkownik'
+    },
+    {
+        key: 'RESERVATION',
+        value: 'Rezerwacja'
+    },
+    {
+        key: 'USERS',
+        value: 'Użytkownicy'
+    },
+    {
+        key: 'ADDRESS',
+        value: 'Adres'
+    },
+    {
+        key: 'EDIT',
+        value: 'Edycja'
+    },
+    {
+        key: 'ADD_USER',
+        value: 'Dodaj Użytkownika'
+    },
+    {
+        key: 'EMAIL',
+        value: 'Email'
+    },
+    {
+        key: 'FIRST_NAME',
+        value: 'Imie'
+    },
+    {
+        key: 'LAST_NAME',
+        value: 'Nazwisko'
+    },
+    {
+        key: 'PHONE',
+        value: 'Telefon'
+    },
+    {
+        key: 'PASSWORD',
+        value: 'Hasło'
+    },
+    {
+        key: 'REPEAT_PASSWORD',
+        value: 'Powtórz Hasło'
+    },
+    {
+        key: 'EDIT_USER',
+        value: 'Edycja Użytkownika'
+    },
+    {
+        key: 'ROLE',
+        value: 'Uprawnienia'
+    },
+    {
+        key: 'ADD_RESERVATION',
+        value: 'Dodaj Rezerwacje'
+    },
+    {
+        key: 'DATE_FROM',
+        value: 'Od Kiedy'
+    },
+    {
+        key: 'DATE_TO',
+        value: 'Do Kiedy'
+    },
+    {
+        key: 'SELECT_MULTIPLE_OPTIONS',
+        value: 'Wybierz Wiele Opcji'
+    },
+    {
+        key: 'ITEMS',
+        value: 'Pozycje'
+    },
+    {
+        key: 'SELECTED',
+        value: 'Wybrane'
+    },
+    {
+        key: 'PAGINATION_PREV',
+        value: 'Cofnij'
+    },
+    {
+        key: 'PAGINATION_NEXT',
+        value: 'Dalej'
+    },
+    {
+        key: 'EMPTY_LIST',
+        value: 'Pusta Lista'
+    },
+    {
+        key: 'CREATED',
+        value: 'Utworzono'
+    },
+    {
+        key: 'NEXT_STATUS',
+        value: 'Zmień Status'
+    },
+    {
+        key: 'PROCESSING',
+        value: 'W trakcie'
+    },
+    {
+        key: 'COMPLETED',
+        value: 'Ukończone'
+    },
+    {
+        key: 'ORDER',
+        value: 'Zamówienie'
+    },
+    {
+        key: 'PRICE',
+        value: 'Cena'
+    },
+    {
+        key: 'SELECT_INGREDIENTS',
+        value: 'Wybierz Składniki'
+    },
+    {
+        key: 'LEVEL',
+        value: 'Poziom'
+    },
+    {
+        key: 'SEATS',
+        value: 'Miejsce'
+    },
+    {
+        key: 'REMOVE_OBJECT',
+        value: 'Usuń Pozycję'
+    },
+    {
+        key: 'REMOVE_OBJECT_CONFIRM',
+        value: 'Potwierdź Usunięcie Pozycji'
+    },
+    {
+        key: 'ADD_ORDER',
+        value: 'Dodaj Zamówienie'
+    },
+    {
+        key: 'MENU_ORDERS_EMPTY',
+        value: 'Zamówienie Puste'
+    },
+    {
+        key: 'ORDER_DISH',
+        value: 'Zamów Danie'
+    },
+    {
+        key: 'MAKE_ORDER',
+        value: 'Utwórz Zamówienie'
+    },
+    {
+        key: 'REMOVE_DISH',
+        value: 'Usuń Danie'
+    },
+    {
+        key: 'CONFIRM_PAYMENT',
+        value: 'Potwierdź Płatność'
+    },
+    {
+        key: 'PAY',
+        value: 'Zapłać'
+    }
+];
